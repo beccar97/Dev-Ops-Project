f188fcec440800064e98e9c2a67008bae6ba3a84
Becky Carter <becky.carter@softwire.com>
2021-04-28 17:43:57 +0100

    12.3 - Use Azure blob storage as remote state

--- main.tf
+++ main.tf
@@ -1,4 +1,11 @@
 terraform {
+  backend "azurerm" {
+    resource_group_name = "SoftwirePilot_BeckyCarter_ProjectExercise"
+    storage_account_name = "beccarterraformstate5033"
+    container_name = "beccarterraformstate"
+    key = "terraform.tfstate"
+  }
+
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
