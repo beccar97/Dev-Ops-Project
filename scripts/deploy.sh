5183bb256208d077b72d71b46a54ff0984486f98
Becky Carter <becky.carter@softwire.com>
2021-05-06 17:39:50 +0100

    12.4 try and fix webhook url in deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,4 +6,6 @@ echo 'Pushing to docker hub...'
 docker push $DOCKER_USERNAME/$DOCKER_IMAGE_NAME:latest 
 docker push $DOCKER_USERNAME/$DOCKER_IMAGE_NAME:$TRAVIS_COMMIT
 echo 'Posting to Webhook...'
+echo "$(terraform output -raw webhook_url)"
+echo $(terraform output -raw webhook_url)
 curl -dH -X POST "$(terraform output -raw webhook_url)"
