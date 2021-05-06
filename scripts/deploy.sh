fc2b3033f72dab1adee3410687e823054db45afc
Becky Carter <becky.carter@softwire.com>
2021-05-06 15:35:09 +0100

    12.4 - Update travis deployment to use terraform

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,4 +6,4 @@ echo 'Pushing to docker hub...'
 docker push $DOCKER_USERNAME/$DOCKER_IMAGE_NAME:latest 
 docker push $DOCKER_USERNAME/$DOCKER_IMAGE_NAME:$TRAVIS_COMMIT
 echo 'Posting to Webhook...'
-curl -dH -X POST $WEBHOOK_URL
+curl -dH -X POST "$(terraform output -raw webhook_url)"
